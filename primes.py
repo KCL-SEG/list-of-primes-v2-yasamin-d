a7cef1e595e4cb0942681265a2a875acb4315377
yasamin-d <yasamindivsalar@gmail.com>
2022-12-08 20:38:21 +0000

    implement prime list

--- primes.py
+++ primes.py
@@ -2,5 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    prime_numbers = []
+
+    if (number_of_primes <= 0):
+        raise ValueError('number must be non-negative')
+    else:
+        for number in range(2, 100):
+            is_prime = all((number % num != 0) for num in range(2, (number)))
+            if is_prime:
+                prime_numbers.append(number)
+            if len(prime_numbers) == number_of_primes:
+                break
+        return prime_numbers
